f5351f05db107a3b20be4b002118746e2bcc5756
marc7s <marcus.schagerberg@gmail.com>
2021-03-13 19:21:02 +0100

    Added additional image

--- commands/scheduled/birthday.js
+++ commands/scheduled/birthday.js
@@ -14,22 +14,22 @@ function handle(bot){
                             let today = new Date();
                 
                             if(date.getMonth() == today.getMonth() && date.getDate() == today.getDate()){
-                                    if(guildConfig.config.id == member.guild.id && guildConfig.birthdayChannel){
-                                        let channelId = guildConfig.birthdayChannel;
-                                        let channel = guild.channels.cache.get(channelId);
-                                
-                                        let randPepe = getRandomImage(config.bot.URIs.pepeURI);
-                        
-                                        const pepe = new MessageAttachment(randPepe);
-                        
-                                        let years = today.getFullYear() - date.getFullYear();
+                                if(guildConfig.config.id == member.guild.id && guildConfig.birthdayChannel){
+                                    let channelId = guildConfig.birthdayChannel;
+                                    let channel = guild.channels.cache.get(channelId);
+                            
+                                    let randPepe = getRandomImage(config.bot.URIs.pepeURI);
+                    
+                                    const pepe = new MessageAttachment(randPepe);
+                    
+                                    let years = today.getFullYear() - date.getFullYear();
 
-                                        const msg = '*Happy Birthday ' + member.toString() + '* :heart: \n*You are now ' + years + ' years old*';
-                                        
-                                        channel.send(msg, pepe);
-                                        console.log('Congratulated ' + member.user.username + ' on becoming ' + years + ' years old');
-                        
-                                    }
+                                    const msg = '*Happy Birthday ' + member.toString() + '* :heart: \n*You are now ' + years + ' years old*';
+                                    
+                                    channel.send(msg, pepe);
+                                    console.log('Congratulated ' + member.user.username + ' on becoming ' + years + ' years old');
+                    
+                                }
                             }
                         }            
                     });
--- commands/text/info.js
+++ commands/text/info.js
@@ -35,24 +35,32 @@ const chConfig = new CommandHandlerConfig(
                         lastMsgString = '`' + lastMsg.content + '` in ' + lastMsg.channel.toString();
                     }
                 }
+                let nickname = (member.nickname) ? member.nickname : '*No nickname*';
+                let hexColor = (member.displayHexColor) ? member.displayHexColor : '#99AAB5';
 
                 let data = {
                     birthday: getDate(birthday),
                     lastMessage: lastMsgString,
                     username: member.user.username,
+                    nickname: nickname,
                     avatarUrl: member.user.displayAvatarURL(),
                     tag: member.user.tag,
-                    id: member.user.id
+                    id: member.user.id,
+                    joinedGuild: getDate(member.joinedAt),
+                    displayColor: hexColor
                 };
 
                 const embed = new MessageEmbed()
                         .setTitle(data.username)
+                        .setColor(data.displayColor)
                         .addFields(
                             { name: 'Username', value: data.username },
+                            { name: 'Nickname', value: data.nickname },
                             { name: 'Tag', value: data.tag },
                             { name: 'ID', value: data.id },
                             { name: 'Avatar URL', value: data.avatarUrl },
                             { name: 'Birthday', value: data.birthday },
+                            { name: 'Joined ' + message.guild.name, value: data.joinedGuild },
                             { name: 'Last message', value: data.lastMessage }
                         )
                         .setImage(data.avatarUrl);
