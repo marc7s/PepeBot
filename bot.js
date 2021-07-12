ab4def187387658737cd69274f1cdec6b02088e7
marc7s <marcus.schagerberg@gmail.com>
2021-07-12 19:45:53 +0200

    Added single instance

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const env = require('./.env.js');
 const path = require('path');
 const { checkHandlers } = require('./configs/handler-config');
 const schedule = require('node-schedule');
+const SingleInstance = require('single-instance');
+const locker = new SingleInstance('PepeBot');
 
 
 var birthdayHandler = require('./commands/scheduled/birthday');
@@ -32,36 +34,40 @@ const intents = new Intents([
     "GUILD_MEMBERS"
 ]);
 
-const bot = new Client({ws: { intents }});
+locker.lock().then(() => {
+    const bot = new Client({ws: { intents }});
 
-bot.once('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.user.username + ' - (' + bot.user.id + ')');
+    bot.once('ready', function (evt) {
+        logger.info('Connected');
+        logger.info('Logged in as: ');
+        logger.info(bot.user.username + ' - (' + bot.user.id + ')');
 
-    bot.user.setPresence(env.config.bot.presence);
-});
-
-bot.login(env.config.discord.token);
-
-// SECOND MINUTE HOUR DAY_OF_MONTH MONTH DAY_OF_WEEK
-schedule.scheduleJob('0 0 0 * * *', () => { birthdayHandler.handle(bot); });
-
-bot.on('message', message => {
-    Object.keys(env.config.guilds).forEach(async key => {
-        let guild = env.config.guilds[key];
-        if(guild.config.id == message.guild.id){
-
-            const cmdChar = guild.config.cmdChar;
-            const cmdCharLen = cmdChar.length;
-            let args = message.content.substring(cmdCharLen).split(' ');
-            let firstChar = message.content.substring(0, cmdCharLen);
-            let cmd = args[0];
-            args.shift();
-
-            checkHandlers(firstChar, cmd, args, message, guild);            
-        }
+        bot.user.setPresence(env.config.bot.presence);
     });
+
+    bot.login(env.config.discord.token);
+
+    // SECOND MINUTE HOUR DAY_OF_MONTH MONTH DAY_OF_WEEK
+    schedule.scheduleJob('0 0 0 * * *', () => { birthdayHandler.handle(bot); });
+
+    bot.on('message', message => {
+        Object.keys(env.config.guilds).forEach(async key => {
+            let guild = env.config.guilds[key];
+            if(guild.config.id == message.guild.id){
+
+                const cmdChar = guild.config.cmdChar;
+                const cmdCharLen = cmdChar.length;
+                let args = message.content.substring(cmdCharLen).split(' ');
+                let firstChar = message.content.substring(0, cmdCharLen);
+                let cmd = args[0];
+                args.shift();
+
+                checkHandlers(firstChar, cmd, args, message, guild);            
+            }
+        });
+    });
+}).catch(err => {
+    console.error("PepeBot is already running");
 });
 
 
